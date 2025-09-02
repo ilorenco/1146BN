8e690a5f502cda2b27e349d15ed757d011dd8686
compiani-univille <eduardocompiani@univille.br>
2025-09-02 22:16:02 +0000

    Permitir acesso remoto do H2

--- auth-service/src/main/java/com/example/auth_service/infrastructure/security/SecurityConfig.java
+++ auth-service/src/main/java/com/example/auth_service/infrastructure/security/SecurityConfig.java
@@ -23,7 +23,9 @@ public class SecurityConfig {
                 )
                 .httpBasic(AbstractHttpConfigurer::disable)
                 .formLogin(AbstractHttpConfigurer::disable)
-                .cors(Customizer.withDefaults());
+                .cors(Customizer.withDefaults())
+                .headers(headers -> headers.frameOptions(frame -> frame.sameOrigin())
+            );;
 
         return http.build();
     }
